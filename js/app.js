b6257bb0a1d787078100a98145a6c323fb15131d
kkikz <kristian.sporina.zz@gmail.com>
2022-02-03 13:56:51 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+//Scroll up button
+  //Get the button:
+  mybutton = document.getElementById("myBtn");
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function() {scrollFunction()};
+  
+  function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+
+/*
+  //Parallax
+$(window).scroll(function () { 
+    $('#prlx').css({
+        'bottom' : -($(this).scrollTop()/10)+"px" // increase # to make even slower
+    }); 
+ });
+*/
+
