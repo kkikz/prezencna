326fa11b0afff34fe7ca1ca7c7c93aee43fcf69f
kkikz <kristian.sporina.zz@gmail.com>
2022-02-18 19:03:10 +0100

    dfafa

--- js/app.js
+++ js/app.js
@@ -1,32 +1,44 @@
 //Scroll up button
-  //Get the button:
-  mybutton = document.getElementById("myBtn");
+//Get the button:
+mybutton = document.getElementById("myBtn");
 
-  // When the user scrolls down 20px from the top of the document, show the button
-  window.onscroll = function() {scrollFunction()};
-  
-  function scrollFunction() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-      mybutton.style.display = "block";
-    } else {
-      mybutton.style.display = "none";
-    }
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-  
-  // When the user clicks on the button, scroll to the top of the document
-  function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /*
-  //Parallax
+//Parallax
 $(window).scroll(function () { 
-    $('#prlx').css({
-        'bottom' : -($(this).scrollTop()/10)+"px" // increase # to make even slower
-    }); 
- });
+  $('#prlx').css({
+    'bottom' : -($(this).scrollTop()/10)+"px" // increase # to make even slower
+  }); 
+});
 */
 
 
-//Gallery carousel apartman
+//gallery
+$(function() {
+  var selectedClass = "";
+  $(".filter").click(function(){
+    selectedClass = $(this).attr("data-rel");
+    $("#gallery").fadeTo(100, 0.1);
+    $("#gallery > div").not("."+selectedClass).fadeOut().removeClass('animation');
+    setTimeout(function() {
+      $("."+selectedClass).fadeIn().addClass('animation');
+      $("#gallery").fadeTo(300, 1);
+    }, 300);
+  });
+});
